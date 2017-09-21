a10ee12bc59a42ed443f32c4f472133a11f53dfa
Tri <zidanecr7kaka@gmail.com>
2017-09-22 01:51:19 +0700

    tes

---
+++ .env/Scripts/pip3.6-script.py
@@ -0,0 +1,12 @@
+#!"G:\Belajar\Belajar Program & Aplikasi\PYTHON\Pythonthusiast\Python-for-remote-work\WEB SCRAPING\jadwal-sholat-payakumbuh\.env\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==9.0.1','console_scripts','pip3.6'
+__requires__ = 'pip==9.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==9.0.1', 'console_scripts', 'pip3.6')()
+    )
